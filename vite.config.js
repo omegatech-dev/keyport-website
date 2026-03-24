e7116b82adb1ba753abfd5fd6b64873feee0c8ce
Sergio Peralta <serpel.js@hotmail.com>
2026-03-24 01:36:16 -0600

    fix: align vite configuration and version precisely with Alimentos San Geronimo

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/keyport-website/',
 })
